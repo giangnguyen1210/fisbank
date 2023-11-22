f2a0297ff527588187b458ca72a7efa6cdb41147
nxhuy1697 <nxhuy1697@gmail.com>
2023-11-22 10:31:02 +0700

    git ignore

--- src/main/java/com/fisbank/controller/AuthController.java
+++ src/main/java/com/fisbank/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.fisbank.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-
+    @PostMapping("/test")
+    public ResponseEntity<?> test(){
+        return null;
+    }
 }
