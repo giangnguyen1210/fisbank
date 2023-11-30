b82622157d182b6e18eef02e3fdd0b63b3ad2294
hong.nt1 <giangnguyen.011210@gmail.com>
2023-11-30 17:48:32 +0700

    update time expire

--- src/main/java/com/fisbank/security/SecurityConstants.java
+++ src/main/java/com/fisbank/security/SecurityConstants.java
@@ -1,6 +1,5 @@
 package com.fisbank.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 1000*60*24;
-    public static final String JWT_SECRET = "secret";
+    public static final long JWT_EXPIRATION = 1000*60*20;
 }
